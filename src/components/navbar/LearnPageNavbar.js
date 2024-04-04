83dd4b823db9f9d352f8f203871a29451eb9631c
phanh247 <phuonganh24799@gmail.com>
2024-04-05 03:29:55 +0700

    fixed learn page navbar, doing learn page sub nav

--- src/components/navbar/LearnPageNavbar.js
+++ src/components/navbar/LearnPageNavbar.js
@@ -4,13 +4,34 @@ import Diamond from '../../images/diamond.svg';
 import Heart from '../../images/heart.svg';
 import Energy from '../../images/energy.svg';
 import User from '../../images/user.svg';
+import JapanFlag from '../../images/japan-flag.svg';
+import SpainFlag from '../../images/spain-flag.svg';
 
 const LearnPageNavbar = () => {
     return (
-        <div className='learn-page-navbar'>
+        <div className='learn-page-navbar w-5/6 absolute right-0'>
             <ul className="learn-page-navbar__nav-list flex items-center justify-end px-12 py-6">
-                <li className="learn-page-navbar__nav-item">
+                <li className="learn-page-navbar__nav-item relative">
                     <img src={ChinaFlag} alt="" />
+                    <div className="learn-page-navbar__flag-list-container">
+                        <ul className="learn-page-navbar__flag-list">
+                            <li className="learn-age-navbar__flag-item">
+                                <a href="">
+                                    <img src={JapanFlag} alt="" /> 
+                                </a>
+                            </li>
+                            <li className="learn-age-navbar__flag-item">
+                                <a href="">
+                                    <img src={SpainFlag} alt="" />
+                                </a>
+                            </li>
+                            <li className="learn-age-navbar__flag-item">
+                                <a href="">
+                                    
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
                 </li>
                 <li className="learn-page-navbar__nav-item">
                     <img src={Diamond} alt="" />
--- src/components/navbar/LearnPageSideNavbar.js
+++ src/components/navbar/LearnPageSideNavbar.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const LearnPageSideNavbar = () => {
     return (
-        <div className='learn-page-side-navbar'>
+        <div className='learn-page-side-navbar w-1/6 bg-[#667abe] h-screen z-10'>
             
         </div>
     );
