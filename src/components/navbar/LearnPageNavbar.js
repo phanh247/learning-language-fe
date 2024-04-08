f4d90b6a737200250aa2b17eee8b70d9aec6d55a
phanh247 <phuonganh24799@gmail.com>
2024-04-08 20:56:32 +0700

    doing learn page nav & side nav

--- src/components/navbar/LearnPageNavbar.js
+++ src/components/navbar/LearnPageNavbar.js
@@ -6,28 +6,32 @@ import Energy from '../../images/energy.svg';
 import User from '../../images/user.svg';
 import JapanFlag from '../../images/japan-flag.svg';
 import SpainFlag from '../../images/spain-flag.svg';
+import AddIcon from '../../images/plus.svg';
 
 const LearnPageNavbar = () => {
     return (
         <div className='learn-page-navbar w-5/6 absolute right-0'>
             <ul className="learn-page-navbar__nav-list flex items-center justify-end px-12 py-6">
-                <li className="learn-page-navbar__nav-item relative">
+                <li className="learn-page-navbar__nav-item learn-page-nav__course relative">
                     <img src={ChinaFlag} alt="" />
-                    <div className="learn-page-navbar__flag-list-container">
+                    <div className="learn-page-navbar__flag-list-container c-shadow">
                         <ul className="learn-page-navbar__flag-list">
                             <li className="learn-age-navbar__flag-item">
                                 <a href="">
                                     <img src={JapanFlag} alt="" /> 
+                                    <span className='block'>Japanese</span>
                                 </a>
                             </li>
                             <li className="learn-age-navbar__flag-item">
                                 <a href="">
                                     <img src={SpainFlag} alt="" />
+                                    <span className='block'>Spanish</span>
                                 </a>
                             </li>
                             <li className="learn-age-navbar__flag-item">
                                 <a href="">
-                                    
+                                    <img src={AddIcon} alt="" />
+                                    <span className='block text-[14px]'>Add new course</span>
                                 </a>
                             </li>
                         </ul>
--- src/components/navbar/LearnPageSideNavbar.js
+++ src/components/navbar/LearnPageSideNavbar.js
@@ -1,9 +1,57 @@
 import React from 'react';
+import LearnIcon from '../../images/learn-menu-icon.svg';
+import ExerciseIcon from '../../images/exercise-menu-icon.svg';
+import ReviewIcon from '../../images/review-menu-icon.svg';
+import LeaderBoard from '../../images/leaderboard-menu-icon.svg';
+import MoreIcon from '../../images/more-menu-icon.svg';
+
 
 const LearnPageSideNavbar = () => {
     return (
         <div className='learn-page-side-navbar w-1/6 bg-[#667abe] h-screen z-10'>
-            
+            <ul className='learn-page-side-nav__list'>
+                <li className='learn-page-side-nav__item'>
+                    <a href="" className='learn-page-side-nav__link text-white text-2xl my-0 mx-auto'>
+                        iLinguist
+                    </a>
+                    
+                </li>
+                <li className='learn-page-side-nav__item'>
+                    <a href="" className='learn-page-side-nav__link'>
+                        <img src={LearnIcon} alt="" />
+                        <span className='text-white'>LEARN</span>
+                    </a>
+
+                </li>
+                <li className='learn-page-side-nav__item'>
+                    <a href="" className='learn-page-side-nav__link'>
+                        <img src={ExerciseIcon} alt="" />
+                        <span className='text-white'>EXERCISE</span>
+                    </a>
+
+                </li>
+                <li className='learn-page-side-nav__item'>
+                    <a href="" className='learn-page-side-nav__link'>
+                        <img src={ReviewIcon} alt="" />
+                        <span className='text-white'>REVIEW</span>
+                    </a>
+
+                </li>
+                <li className='learn-page-side-nav__item'>
+                    <a href="" className='learn-page-side-nav__link'>
+                        <img src={LeaderBoard} alt="" />
+                        <span className='text-white'>LEADERBOARD</span>
+                    </a>
+
+                </li>
+                <li className='learn-page-side-nav__item'>
+                    <a href="" className='learn-page-side-nav__link'>
+                        <img src={MoreIcon} alt="" />
+                        <span className='text-white'>MORE</span>
+                    </a>
+
+                </li>
+            </ul>
         </div>
     );
 };
