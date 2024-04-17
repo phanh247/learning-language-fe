c2a4b25a616ad2082a4eafef5ee5055f730784c8
phanh247 <phuonganh24799@gmail.com>
2024-04-18 00:15:13 +0700

    create learn page

--- src/App.js
+++ src/App.js
@@ -1,22 +1,11 @@
 import './App.css';
-import { Routes, Route } from "react-router-dom";
-import FlashCard from './components/flashcard/FlashCard';
-import Navbar from './components/navbar/Navbar';
-import CourseMainHomepage from './components/course-page/CourseHomepage';
-import ChoosePathPage from './components/learn-pages/ChoosePathPage';
-import LessonPage from './components/learn-pages/LessonPage';
-import LoginForm from './components/login/LoginForm';
 import CourseHomepage from './components/course-page/CourseHomepage';
-import MainHomepage from './components/landing-page/MainHomepage';
 
 function App() {
   return (
     <div className="App">
-      {/* <Routes>
-        <Route index   path="/" element={<CourseHomepage />}/>
-        <Route path='/login' element={<LoginForm />} />
-      </Routes> */}
-      <MainHomepage />
+
+      <CourseHomepage />
     </div>
   );
 }
