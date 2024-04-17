3d90874967580c4cbd62a32ec7d820a33adbb8db
phanh247 <phuonganh24799@gmail.com>
2024-04-17 14:13:52 +0700

    finish course selection section

--- src/components/home-page/CourseSelection.js
+++ src/components/home-page/CourseSelection.js
@@ -19,55 +19,58 @@ const CourseSelection = () => {
         <div className='course-selection px-[7%] py-[3%]'>
             <h2 className='course-selection__title text-3xl mb-20 font-bold'>Which <span className='text-[#667abe]'>language</span> do you want to learn?</h2>
             
-            <div class="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
-                <a href="#" className="group">
-                    <div className="">
-                        <img src={UKFlag} alt="UK circle flag" className="h-full w-1/3 object-cover object-center group-hover:opacity-75 my-0 mx-auto" />
-                    </div>
-                    <h3 className="text-base font-semibold">English</h3>
-                </a>
-                <a href="#" className="group">
-                    <div className="">
-                        <img src={ChinaFlag} alt="UK circle flag" className="h-full w-1/3 object-cover object-center group-hover:opacity-75 my-0 mx-auto" />
-                    </div>
-                    <h3 className="text-base font-semibold">Chinese</h3>
-                </a>
-                <a href="#" className="group">
-                    <div className="">
-                        <img src={JapanFlag} alt="UK circle flag" className="h-full w-1/3 object-cover object-center group-hover:opacity-75 my-0 mx-auto" />
-                    </div>
-                    <h3 className="text-base font-semibold">Japanese</h3>
-                </a>
-                <a href="#" className="group">
-                    <div className="">
-                        <img src={KoreaFlag} alt="UK circle flag" className="h-full w-1/3 object-cover object-center group-hover:opacity-75 my-0 mx-auto" />
-                    </div>
-                    <h3 className="text-base font-semibold">Korean</h3>
-                </a>
-                <a href="#" className="group">
-                    <div className="">
-                        <img src={SpainFlag} alt="UK circle flag" className="h-full w-1/3 object-cover object-center group-hover:opacity-75 my-0 mx-auto" />
-                    </div>
-                    <h3 className="text-base font-semibold">Spanish</h3>
-                </a>
-                <a href="#" className="group">
-                    <div className="">
-                        <img src={GermanyFlag} alt="UK circle flag" className="h-full w-1/3 object-cover object-center group-hover:opacity-75 my-0 mx-auto" />
-                    </div>
-                    <h3 className="text-base font-semibold">German</h3>
-                </a>
-                <a href="#" className="group">
-                    <div className="">
-                        <img src={FranceFlag} alt="UK circle flag" className="h-full w-1/3 object-cover object-center group-hover:opacity-75 my-0 mx-auto" />
-                    </div>
-                    <h3 className="text-base font-semibold">French</h3>
-                </a>
-                <a href="#" className="group">
-                    <div className="">
-                        <img src={RussiaFlag} alt="UK circle flag" className="h-full w-1/3 object-cover object-center group-hover:opacity-75 my-0 mx-auto" />
-                    </div>
-                    <h3 className="text-base font-semibold">Russian</h3>
-                </a>
+            <div className="course-selection__grid-wrapper grid grid-cols-6 gap-x-2 gap-y-10">
+                <div className="course-selection__grid-item">
+                    <img src={UKFlag} className='w-1/2 my-0 mx-auto cursor-pointer hover:shadow-md ' alt="" />
+                    <span className='block font-bold text-lg'>English</span>
+                </div>
+                <div className="course-selection__grid-item">
+                    <img src={VietnamFlag} className='w-1/2 my-0 mx-auto cursor-pointer hover:shadow-md ' alt="" />
+                    <span className='block font-bold text-lg'>Vietnamese</span>
+                </div>
+                <div className="course-selection__grid-item">
+                    <img src={ChinaFlag} className='w-1/2 my-0 mx-auto cursor-pointer hover:shadow-md ' alt="" />
+                    <span className='block font-bold text-lg'>Simplified Chinese</span>
+                </div>
+                <div className="course-selection__grid-item">
+                    <img src={JapanFlag} className='w-1/2 my-0 mx-auto cursor-pointer hover:shadow-md ' alt="" />
+                    <span className='block font-bold text-lg'>Japanese</span>
+                </div>
+                <div className="course-selection__grid-item">
+                    <img src={KoreaFlag} className='w-1/2 my-0 mx-auto cursor-pointer hover:shadow-md ' alt="" />
+                    <span className='block font-bold text-lg'>Korean</span>
+                </div>
+                <div className="course-selection__grid-item">
+                    <img src={TaiwanFlag} className='w-1/2 my-0 mx-auto cursor-pointer hover:shadow-md ' alt="" />
+                    <span className='block font-bold text-lg'>Traditional Chinese</span>
+                </div>
+                <div className="course-selection__grid-item">
+                    <img src={GermanyFlag} className='w-1/2 my-0 mx-auto cursor-pointer hover:shadow-md ' alt="" />
+                    <span className='block font-bold text-lg'>German</span>
+                </div>
+                <div className="course-selection__grid-item">
+                    <img src={SpainFlag} className='w-1/2 my-0 mx-auto cursor-pointer hover:shadow-md ' alt="" />
+                    <span className='block font-bold text-lg'>Spanish</span>
+                </div>
+                <div className="course-selection__grid-item">
+                    <img src={FranceFlag} className='w-1/2 my-0 mx-auto cursor-pointer hover:shadow-md ' alt="" />
+                    <span className='block font-bold text-lg'>French</span>
+                </div>
+                <div className="course-selection__grid-item">
+                    <img src={ItalyFlag} className='w-1/2 my-0 mx-auto cursor-pointer hover:shadow-md ' alt="" />
+                    <span className='block font-bold text-lg'>Italian</span>
+                </div>
+                <div className="course-selection__grid-item">
+                    <img src={RussiaFlag} className='w-1/2 my-0 mx-auto cursor-pointer hover:shadow-md ' alt="" />
+                    <span className='block font-bold text-lg'>Russian</span>
+                </div>
+                <div className="course-selection__grid-item">
+                    <img src={BrazilFlag} className='w-1/2 my-0 mx-auto cursor-pointer hover:shadow-md ' alt="" />
+                    <span className='block font-bold text-lg'>Portuguese</span>
+                </div>
+                <div className="course-selection__grid-item">
+                    
+                </div>
             </div>
         </div>
     );
